e0017e14e8052136217cc19f9c465d122d4ae1be
Sedef Cakmak <cakmaksedef@gmail.com>
2021-09-13 17:15:14 +0300

    Corrected config/routes.db and corrected partyanimals controller file name

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :partyanimals, only: [ :index, :show, :new, :create ] do
-  resources :bookmarks, only: [ :create, :new ]
-  end
+  resources :partyanimals, only: [ :index, :show, :new, :create ]
   resources :bookings, only: :destroy
 end
