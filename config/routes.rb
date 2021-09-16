2e4ec88969a1ebe12d7416cfb2fd10fc3a1c4191
Sedef Cakmak <cakmaksedef@gmail.com>
2021-09-16 22:46:48 +0300

    Completed edit for my animals

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
-  resources :party_animals, only: [ :index, :show, :new, :create ] do
+
+  resources :party_animals, only: [ :index, :show, :new, :create, :edit, :update ] do
     resources :bookings, only: [ :new, :create ]
   end
   resources :bookings, only: [ :destroy ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_081023) do
+ActiveRecord::Schema.define(version: 2021_09_16_181413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,10 +56,10 @@ ActiveRecord::Schema.define(version: 2021_09_16_081023) do
   create_table "party_animals", force: :cascade do |t|
     t.string "name"
     t.integer "age"
-    t.string "main_interest"
-    t.string "secondary_interest"
+    t.text "main_interest"
+    t.text "secondary_interest"
     t.integer "price"
-    t.string "description"
+    t.text "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
